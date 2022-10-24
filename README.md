1eca4972b7253ab620a22238decd4be17a76552a
Sebastian Avila A <sebastian4j@gmail.com>
2022-10-24 17:32:10 -0300

    test pendientes agregados
    readme agregado

---
+++ README.md
@@ -0,0 +1,3 @@
+# postgresqlgo
+- Entrega la estructura **Postgresqlgo** para tener conexiónes con postgresql usando un pool de conexiones usando las variables de ambiente POSTGRES_PORT,
+POSTGRES_HOST, POSTGRES_USER, POSTGRES_DB y POSTGRES_PASSWORD.
--- postgresqlgo_test.go
+++ postgresqlgo_test.go
@@ -1,7 +1,69 @@
 package postgresqlgo
 
-import "testing"
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
 
-func TestConnection(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
 
+func TestConnections(t *testing.T) {
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "postgres:10-alpine",
+		ExposedPorts: []string{"5432"},
+		WaitingFor:   wait.ForListeningPort("5432/tcp"),
+		Env: map[string]string{
+			"POSTGRES_USER":             "postgres",
+			"POSTGRES_PASSWORD":         "postgres",
+			"POSTGRES_DB":               "postgres",
+			"POSTGRES_HOST_AUTH_METHOD": "trust",
+		},
+	}
+	rc, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	mappedPort, _ := rc.MappedPort(ctx, "5432")
+	hostIP, _ := rc.Host(ctx)
+	log.Printf("%s:%s", hostIP, mappedPort.Port())
+	os.Setenv("POSTGRES_HOST", hostIP)
+	os.Setenv("POSTGRES_PORT", mappedPort.Port())
+	os.Setenv("POSTGRES_USER", "postgres")
+	os.Setenv("POSTGRES_PASSWORD", "postgres")
+	os.Setenv("POSTGRES_DB", "postgres")
+
+	t.Run("puedo hacer una query", func(t *testing.T) {
+		p := Postgresqlgo{}
+		con := p.Conn()
+		r, err := con.Query(context.Background(), "select extract(epoch from now())::int")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer r.Close()
+		var now int
+		for r.Next() {
+			err := r.Scan(&now)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		assert.Greater(t, now, 1641042061)
+		con.Release()
+	})
+
+	t.Run("puedo establecer conexiones", func(t *testing.T) {
+		p := Postgresqlgo{}
+		for a := 0; a < 10_000; a++ {
+			con := p.Conn()
+			con.Release()
+		}
+	})
 }
